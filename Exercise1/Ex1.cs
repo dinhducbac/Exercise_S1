6160d504462b61977b035a274740c2392e929482
dinhducbac <dinhducbac1998@gmail.com>
2022-07-06 16:53:45 +0700

    up code

---
+++ Exercise1/Ex1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Ex1
+    {
+        public string sSportCar = "";
+        public Ex1()
+        {
+            var lstSportCar = new List<SportCar>()
+            {
+                new SportCar() { tradeMark = "HONDA", hoursePower = 200},
+                new SportCar() { tradeMark = "LAMBOGINI", hoursePower = 250},
+                new SportCar() { tradeMark = "TOYOTA", hoursePower = 180},
+                new SportCar() { tradeMark = "BUGATI", hoursePower = 300}
+            };
+            Init(lstSportCar);
+        }
+        public async void Init(List<SportCar> sportCars)
+        {
+            var lst = await GetSportCar(sportCars);
+            foreach (var o in lst)
+            {
+                sSportCar += $"TRADEMARK: {o.tradeMark}, HOURSEPOWER: {o.hoursePower}\n";
+            }
+        } 
+        public async Task<List<SportCar>> GetSportCar(List<SportCar> lstSportCar)
+        {
+            return (from l in lstSportCar
+                    where l.hoursePower >= 200
+                    select l).ToList();
+        }
+        public class Car
+        {
+            public string tradeMark { get; set; }
+        }
+        public class SportCar : Car
+        {
+            public int hoursePower { get; set; }
+
+        }
+    }
+}
---
+++ Exercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Exercise1.Ex1 ex1 = new Exercise1.Ex1();
+        Console.WriteLine(ex1.sSportCar);
+    }
+
+}
