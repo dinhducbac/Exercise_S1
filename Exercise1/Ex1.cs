f11cf48a21ce66d2018e8dd4dbf56e19f1c61b23
dinhducbac <dinhducbac1998@gmail.com>
2022-07-07 09:07:26 +0700

    update

--- Exercise1/Ex1.cs
+++ Exercise1/Ex1.cs
@@ -22,13 +22,13 @@ namespace Exercise1
         }
         public async void Init(List<SportCar> sportCars)
         {
-            var lst = await GetSportCar(sportCars);
+            var lst = await GetStrongerSportCar(sportCars);
             foreach (var o in lst)
             {
                 sSportCar += $"TRADEMARK: {o.tradeMark}, HOURSEPOWER: {o.hoursePower}\n";
             }
         } 
-        public async Task<List<SportCar>> GetSportCar(List<SportCar> lstSportCar)
+        public async Task<List<SportCar>> GetStrongerSportCar(List<SportCar> lstSportCar)
         {
             return (from l in lstSportCar
                     where l.hoursePower >= 200
