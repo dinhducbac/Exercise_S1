134d3dacc574af9152142b14dc61504996a19e53
dinhducbac <dinhducbac1998@gmail.com>
2022-07-07 10:44:21 +0700

    update code

--- Exercise1/Ex1.cs
+++ Exercise1/Ex1.cs
@@ -9,23 +9,28 @@ namespace Exercise1
     public class Ex1
     {
         public string sSportCar = "";
+        public List<SportCar> listSportCar;
         public Ex1()
         {
             var lstSportCar = new List<SportCar>()
             {
-                new SportCar() { tradeMark = "HONDA", hoursePower = 200},
-                new SportCar() { tradeMark = "LAMBOGINI", hoursePower = 250},
-                new SportCar() { tradeMark = "TOYOTA", hoursePower = 180},
-                new SportCar() { tradeMark = "BUGATI", hoursePower = 300}
+                new SportCar("HONDA",200,"RED"),
+                new SportCar("LAMBOGINI", 250,"WHITE"),
+                new SportCar("LAMBOGINI", 450,"YELLOW"),
+                new SportCar("TOYOTA",180,"RED"),
+                new SportCar("BUGATI",300,"BROWN")
             };
             Init(lstSportCar);
         }
         public async void Init(List<SportCar> sportCars)
         {
             var lst = await GetStrongerSportCar(sportCars);
+            sSportCar += $"Count total: {Count(sportCars)}\n";
+            sSportCar += $"Count LAMBOGINI: {Count("LAMBOGINI",sportCars)}\n";
+            sSportCar += "Sport car has hourse power >= 200\n";
             foreach (var o in lst)
             {
-                sSportCar += $"TRADEMARK: {o.tradeMark}, HOURSEPOWER: {o.hoursePower}\n";
+                sSportCar += $"TRADEMARK: {o.tradeMark}, HOURSEPOWER: {o.hoursePower}, COLOR: {o.GetColor()}\n";
             }
         } 
         public async Task<List<SportCar>> GetStrongerSportCar(List<SportCar> lstSportCar)
@@ -34,20 +39,35 @@ namespace Exercise1
                     where l.hoursePower >= 200
                     select l).ToList();
         }
+        public int Count(List<SportCar> lstSportCar)
+        {
+            return lstSportCar.Count();
+        }
+        public int Count(string tradeMark, List<SportCar> lstSportCar)
+        {
+            return lstSportCar.Where(p=>p.tradeMark == tradeMark).Count();
+        }
         public class Car
         {
             public string tradeMark { get; set; }
-            public virtual void Get()
+            public virtual string GetColor()
             {
-
+                return "";
             }
         }
         public class SportCar : Car
         {
             public int hoursePower { get; set; }
-            public override void Get()
+            private string Color { get; set; }
+            public SportCar(string tradeMark,int hoursePower,string color)
             {
-                
+                this.tradeMark = tradeMark;
+                this.hoursePower = hoursePower;
+                this.Color = color;
+            }
+            public override string GetColor()
+            {
+                return Color;
             }
         }
     }
